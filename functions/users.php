8e706fd13751a3ef335181eee15d84cce2965471
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-05-17 16:33:03 +0800

    Login and Regis funtion

---
+++ functions/users.php
@@ -0,0 +1,71 @@
+<?php
+include_once "db_connection.php";
+
+function login($data)
+{
+  global $conn;
+
+  $email = htmlspecialchars($data['email']);
+  $password = htmlspecialchars($data['password']);
+
+  $query = "SELECT * FROM users WHERE email = '$email'";
+  $result = mysqli_query($conn, $query);
+
+  // Email check
+  if (mysqli_num_rows($result) === 1) {
+
+    // Password check
+    $row = mysqli_fetch_assoc($result);
+    if (password_verify($password, $row['password'])) {
+      // session_start();
+      header("Location: home.php");
+      die();
+    }
+  }
+  return false;
+}
+
+
+function registration($data)
+{
+  global $conn;
+
+  $email = stripcslashes($data['email']);
+  $password = mysqli_real_escape_string($conn, $data['password']);
+  $password2 = mysqli_real_escape_string($conn, $data['confirm-password']);
+  $role = 1;
+
+  // Email already exists check
+  $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?");
+  $stmt->bind_param("s", $email);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  if ($result->num_rows > 0) {
+    echo "
+            <script>
+                alert('Email already exists');
+            </script>
+        ";
+    return false;
+  }
+
+
+  // Password confirmation check
+  // if Password does not match will return false
+  if ($password !== $password2) {
+    echo "
+                <script>
+                    alert('Konfirmasi password tidak sesuai');
+                </script>
+            ";
+    return false;
+  }
+
+  // Password hashing 
+  $password = password_hash($password, PASSWORD_DEFAULT);
+
+  $query = "INSERT INTO users VALUES('', '$email', '$password', '$role')";
+  $result = mysqli_query($conn, $query);
+
+  return $result;
+}
